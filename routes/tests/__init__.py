f0dd74e72b59615adf7efbb4a27f79a9f3ea22ad
Alimedhat000 <alisensei000@gmail.com>
2026-02-06 18:53:18 +0200

    chore: cleanup legacy test code

--- routes/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Tests package initialization."""
-from flask import Blueprint
-
-# Create main tests blueprint
-tests_bp = Blueprint("tests", __name__, url_prefix="/api/test")
-
-# Import routes to register them
-from . import general, tremor, drawing, speech
